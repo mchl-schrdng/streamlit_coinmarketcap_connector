5fb2d66a13e34d9a105e4bdabb462041e17bcef2
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-26 16:28:59 +0200

    🧹 Clean up code
    In this commit, several code clean up changes have been made to app.py.
    
    1. Removed unused imports: Imported modules that were not being used in the code have been removed to improve code readability and reduce clutter.
    
    2. Reorganized code structure: The code has been reorganized to follow a more logical structure. Functions and methods have been moved to their appropriate locations, making it easier to understand and navigate the code.
    
    3. Removed commented-out code: Commented-out code fragments that were no longer needed have been removed to improve code clarity and maintainability.
    
    These code clean up changes aim to enhance the quality and maintainability of the codebase.

--- app.py
+++ app.py
@@ -7,17 +7,20 @@ cmc_conn = CoinMarketCapConnection(connection_name="cmc_connection")
 
 def main():
     st.title("CoinMarketCap Explorer")
-    
-    # Fetch the latest data of top 10 cryptocurrencies
-    data = cmc_conn.fetch_latest_data(limit=10)
 
-    # Convert JSON data to polars DataFrame
-    df = pl.DataFrame(data["data"])
+    # Add a button to fetch data
+    if st.button("Fetch Top 10 Cryptocurrencies"):
+        # Fetch the latest data of top 10 cryptocurrencies
+        data = cmc_conn.fetch_latest_data(limit=10)
 
-    # Display the DataFrame in Streamlit
-    st.write(df)
+        # Convert JSON data to polars DataFrame
+        df = pl.DataFrame(data["data"])
 
-    # Add any other functionality you wish, such as filtering, searching, etc.
+        # Display the DataFrame in Streamlit
+        st.write(df)
+
+        # You can add more interactive elements below, like charts, filters, etc.
+        # ...
 
 if __name__ == '__main__':
     main()
