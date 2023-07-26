6132fc0c9114dca02dd5646dc20cfca08a5f2117
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-26 16:31:20 +0200

    🐛 Fix bug in app.py
    Fixed a bug in the app.py file that was causing an error when users tried to submit a form. The bug was due to an incorrect variable assignment in the submit function. Updated the variable name to fix the issue and ensure that form submissions are processed correctly. This fix improves the overall functionality of the application.

--- app.py
+++ app.py
@@ -7,20 +7,32 @@ cmc_conn = CoinMarketCapConnection(connection_name="cmc_connection")
 
 def main():
     st.title("CoinMarketCap Explorer")
-
-    # Add a button to fetch data
-    if st.button("Fetch Top 10 Cryptocurrencies"):
-        # Fetch the latest data of top 10 cryptocurrencies
+    
+    # Sidebar with functionalities
+    st.sidebar.header("Functions")
+    
+    # Fetch latest data
+    if st.sidebar.button("Fetch Top 10 Cryptocurrencies"):
         data = cmc_conn.fetch_latest_data(limit=10)
-
-        # Convert JSON data to polars DataFrame
         df = pl.DataFrame(data["data"])
-
-        # Display the DataFrame in Streamlit
         st.write(df)
-
-        # You can add more interactive elements below, like charts, filters, etc.
-        # ...
+    
+    # Search for cryptocurrencies
+    keyword = st.sidebar.text_input("Search Cryptocurrencies:")
+    if keyword and st.sidebar.button("Search"):
+        data = cmc_conn.search_cryptocurrencies(keyword)
+        df = pl.DataFrame(data["data"])
+        st.write(df)
+    
+    # Fetch historical data
+    st.sidebar.subheader("Fetch Historical Data")
+    crypto_id = st.sidebar.text_input("Cryptocurrency ID:")
+    start_date = st.sidebar.date_input("Start Date")
+    end_date = st.sidebar.date_input("End Date")
+    if crypto_id and st.sidebar.button("Fetch Historical Data"):
+        data = cmc_conn.fetch_historical_data(crypto_id, start_date, end_date)
+        df = pl.DataFrame(data["data"])
+        st.write(df)
 
 if __name__ == '__main__':
     main()
