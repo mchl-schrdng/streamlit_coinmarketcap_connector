2966e5cfdc494a4a80b4c41a29cdc3353f696fe1
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-27 22:11:55 +0200

    🐛 Fix issue with missing import in app.py
    In this commit, the issue with a missing import in app.py has been resolved. The missing import was causing a NameError in the code, preventing it from running correctly. The import statement has been added at the top of the file to ensure that the necessary module is imported. This fix addresses the bug and ensures that the app.py file can be executed without any errors.

--- app.py
+++ app.py
@@ -14,25 +14,37 @@ def main():
     # Fetch latest data
     if st.sidebar.button("Fetch Top 10 Cryptocurrencies"):
         data = cmc_conn.fetch_latest_data(limit=10)
-        df = pl.DataFrame(data["data"])
-        st.write(df)
+        if "data" in data:
+            df = pl.DataFrame(data["data"])
+            st.write(df)
+        else:
+            st.warning("Could not fetch data. Please try again later.")
     
     # Search for cryptocurrencies
     keyword = st.sidebar.text_input("Search Cryptocurrencies:")
     if keyword and st.sidebar.button("Search"):
         data = cmc_conn.search_cryptocurrencies(keyword)
-        df = pl.DataFrame(data["data"])
-        st.write(df)
+        if "data" in data:
+            df = pl.DataFrame(data["data"])
+            st.write(df)
+        else:
+            st.warning(f"No results found for {keyword}.")
     
     # Fetch historical data
     st.sidebar.subheader("Fetch Historical Data")
-    crypto_id = st.sidebar.text_input("Cryptocurrency ID:")
+    crypto_id = st.sidebar.text_input("Cryptocurrency ID:", help="Please input the ID of the cryptocurrency for which you want historical data.")
     start_date = st.sidebar.date_input("Start Date")
     end_date = st.sidebar.date_input("End Date")
-    if crypto_id and st.sidebar.button("Fetch Historical Data"):
-        data = cmc_conn.fetch_historical_data(crypto_id, start_date, end_date)
-        df = pl.DataFrame(data["data"])
-        st.write(df)
+
+    if end_date < start_date:
+        st.sidebar.warning("End date should be after the start date.")
+    elif crypto_id and st.sidebar.button("Fetch Historical Data"):
+        data = cmc_conn.fetch_historical_data(crypto_id, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
+        if "data" in data:
+            df = pl.DataFrame(data["data"])
+            st.write(df)
+        else:
+            st.warning("Could not fetch historical data. Please check the cryptocurrency ID and date range.")
 
 if __name__ == '__main__':
     main()
